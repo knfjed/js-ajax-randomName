1f871ec8497fe9d180f14d34be7cd0a352b17a20
knfjed <knfjed@gmail.com>
2020-03-14 07:37:32 +0900

    名前をオブジェクトでコンソールに取得

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+// リクエストの送信
+fetch("https://uinames.com/api/?region=japan")
+  .then(response => response.json())
+  .then(json => console.log(json));
+
 // addを取得
 // addをクリックされたら以下を実行
 // ul要素を取得
@@ -5,19 +10,7 @@
 document.getElementById("add").addEventListener("click", e => {
   const ul = document.querySelector("ul");
   const li = document.createElement("li");
-  li.textContent = randomName();
+  li.textContent.innerHTML = json();
   ul.appendChild(li);
   event.preventDefault();
 });
-
-// randomNameファンクションが呼び出されたら、以下を実行
-// リクエストの送信
-// 成功ならコンソールにレスポンス内容を表示
-// エラーの場合はエラーとコンソール表示
-function (randomName) { fetch("https://uinames.com/api/?region=japan")
-  .then(response => {
-    console.log(response);
-  })
-  .catch(err => {
-    console.log(err);
-  })};
